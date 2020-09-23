ca89e9e43abe5ed8ee9eb75f9c30b1f4a47d5a90
benjaminclosson1 <benjaminclosson1@gmail.com>
2020-09-23 10:43:30 -0700

    Commit 4 added JS psudo code

---
+++ script/app.js
@@ -0,0 +1,45 @@
+// Create Tamagotchi Class
+    // create sub classes for each tamagotchi
+
+// Create Meter Class
+
+// Create Button Class
+
+// Start Button Function
+    // When clicked promt asks you to name your pet, and gives instructions on how to play the game.
+
+// Prompt Function
+    // After naming pet, press OK to start game.
+        // Name populates into the text box.
+    // When clicked time begins to decrement
+    // When clicked age begins to increment 
+    // When clicked meters begin to increment
+
+// Tamagotchi Functions
+    // after 20 seconds, hatch egg
+        // populate baby yoshi on screen (opacity)
+    // reset timer to 60 seconds
+    // start countdown
+    // after each time the timer reaches zero age increases by 5 years, and new older yoshi appears on screen.
+    // When senior yoshi's times expires switch to sunset backdrop and animate fading into the distance. 
+        // Alert: "As your pet walks quietly into the night, know you have loved and care for them with all your heart."
+
+// Meter Functions
+    // If any of the meter bars reach 10 animate flashing, Alerty "You Have Killed Your Pet!"
+    // Hungry Meter bar increments plus one every two seconds
+    
+    // Bordom Meter bar increments plus two every three seconds
+
+    // Sleepy Meter bar increments plus one every second
+    
+
+// Button Functions
+    // Munchy Time button decrements hungry meter by .25 with every click.
+    // Exercise Time button decrements bordom meter by .1 with every click
+    // Sleepy Time button decrements sleepy meter by 3 with every click
+
+    class Tamagotchi {
+        constructor() {
+            
+        }
+    }
